b107dfe467782f70345890cc576e6ec25ffcf0ad
819605834 <124411012+819605834@users.noreply.github.com>
2026-06-18 20:28:15 +0000

    Created addArrayValue

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // Write your code below
+function addArrayValue(obj, key, val) {
+    obj[key] = val;
+    return true;
+}
+
+var myObj = {};
+
+var myArray = [1, 3];
+
+addArrayValue(myObj, 'myValue', myArray);
+
+console.log(myObj['myValue']); // should print [1, 3]
+
+var myObject2 = {
+    color: 'red'
+}
+addArrayValue(myObj, 'myObject', myObject2);
+console.log(myObj['myObject']);
